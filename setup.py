799a207ffedb031ade26e5e10adc03faab363e38
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2019-06-25 12:02:21 +0100

    inital commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='mapactionpy_arcmap',
+      version='0.1',
+      description='Used to drive ArcMap',
+      url='http://github.com/mapaction/mapactionpy_arcmap',
+      author='MapAction',
+      author_email='github@mapaction.com',
+      license='GPL3',
+      packages=['mapactionpy_arcmap'],
+	  test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
