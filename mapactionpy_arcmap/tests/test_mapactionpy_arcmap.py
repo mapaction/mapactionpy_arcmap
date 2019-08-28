286402fcc6c165893981b7a4889ee2254a532938
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2019-08-28 21:44:41 +0100

    skip most unittests

--- mapactionpy_arcmap/tests/test_mapactionpy_arcmap.py
+++ mapactionpy_arcmap/tests/test_mapactionpy_arcmap.py
@@ -1,20 +1,21 @@
+import unittest
 from unittest import TestCase
 
 
 class TestArcMapRunner(TestCase):
-    def test_alway_fail(self):
-        self.assertTrue(False)
-
     def test_alway_pass(self):
         self.assertTrue(True)
 
+    @unittest.SkipTest
     def test_zoom_to_layer(self):
         self.assertTrue(False, msg='zoom to layer for Main Map')
         self.assertTrue(False,
                         msg='zoom to layer plus margin for Location Map')
 
+    @unittest.SkipTest
     def test_layer_in_dict_but_not_in_mxd(self):
         self.assertTrue(False)
 
+    @unittest.SkipTest
     def test_datasource_does_not_exist(self):
         self.assertTrue(False)
