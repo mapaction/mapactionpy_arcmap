f40320af796576e683534d87c2dcbb88b44fbcc4
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2019-08-28 18:50:10 +0100

    add travis and deploy to pypi

--- setup.py
+++ setup.py
@@ -1,19 +1,40 @@
-from setuptools import setup
+import subprocess
+from setuptools import setup, find_packages
+from os import path, environ
 
 
 def readme():
-    with open('README.md') as f:
+    here = path.abspath(path.dirname(__file__))
+    with open(path.join(here, 'README.md')) as f:
         return f.read()
 
 
+def get_dev_build_number():
+    travis_build = environ.get('TRAVIS_BUILD_NUMBER')
+
+    if travis_build:
+        return '.dev{}'.format(travis_build)
+    else:
+        ver = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
+        return '+local.{}'.format(ver.decode('ascii').strip())
+
+
 setup(name='mapactionpy_arcmap',
-      version='0.1',
+      version='0.2{}'.format(get_dev_build_number()),
       description='Used to drive ArcMap',
+      long_description=readme(),
+      long_description_content_type="text/markdown",
       url='http://github.com/mapaction/mapactionpy_arcmap',
       author='MapAction',
       author_email='github@mapaction.com',
       license='GPL3',
-      packages=['mapactionpy_arcmap'],
-          test_suite='nose.collector',
-      tests_require=['nose'],
-      zip_safe=False)
+      packages=find_packages(),
+      test_suite='unittest',
+      tests_require=['unittest'],
+      zip_safe=False,
+      classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: Microsoft :: Windows",
+      ])
