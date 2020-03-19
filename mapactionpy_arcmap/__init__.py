03d95c81edf331d639b2781c72a7a44fae66ef8a
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2020-03-19 18:34:29 +0000

    update more paths

--- mapactionpy_arcmap/__init__.py
+++ mapactionpy_arcmap/__init__.py
@@ -1,9 +1,9 @@
 # flake8: noqa
-from .label_class import LabelClass
-from .map_layer import MapLayer
-from .layer_properties import LayerProperties
-from .map_recipe import MapRecipe
+# from .label_class import LabelClass
+# from .map_layer import MapLayer
+# from .layer_properties import LayerProperties
+# from .map_recipe import MapRecipe
 from .map_chef import MapChef
-from .map_cookbook import MapCookbook
-from .map_report import MapReport
-from .map_result import MapResult
+# from .map_cookbook import MapCookbook
+# from .map_report import MapReport
+# from .map_result import MapResult
