23806de6bf5f9c182466d77fa574c89ef16a2a43
shurst-ma <shurst@mapaction.org>
2020-04-13 09:26:45 +0100

    Update constructor to use Event

--- mapactionpy_arcmap/tests/test_map_chef.py
+++ mapactionpy_arcmap/tests/test_map_chef.py
@@ -2,9 +2,11 @@ import arcpy
 import os
 # import unittest
 from unittest import TestCase
-
 from mapactionpy_arcmap.map_chef import MapChef
 from mapactionpy_controller.crash_move_folder import CrashMoveFolder
+from mapactionpy_controller.map_cookbook import MapCookbook
+from mapactionpy_controller.event import Event
+from mapactionpy_controller.layer_properties import LayerProperties
 
 
 class TestMapChef(TestCase):
@@ -14,44 +16,40 @@ class TestMapChef(TestCase):
         self.path_to_valid_cmf_des = os.path.join(
             self.parent_dir, 'tests', 'test_data', 'fixture_cmf_description_flat_test.json')
         self.cmf = CrashMoveFolder(self.path_to_valid_cmf_des)
-
+        self.cookBook = MapCookbook(self.cmf.map_definitions)
+        self.event = Event(os.path.join(self.parent_dir, 'tests', 'test_data', 
+                                        'event_description.json')) 
         self.my_mxd_fname = os.path.join(self.parent_dir, 'tests', 'test_data',
                                          'output_arcgis_10_6_reference_landscape_bottom.mxd')
+        self.layerDefinition = LayerProperties(self.cmf, '.lyr')                                       
 
     def test_map_chef_constructor(self):
-        # def __init__(self,
-        #              mxd,
-        #              cookbookJsonFile,
-        #              layerPropertiesJsonFile,
-        #              crashMoveFolder,
-        #              layerDirectory,
-        #              versionNumber=1):
         my_mxd = arcpy.mapping.MapDocument(self.my_mxd_fname)
 
         mc = MapChef(
             my_mxd,
-            self.cmf.map_definitions,
-            self.cmf.layer_properties,
-            self.path_to_valid_cmf_des,
-            self.cmf.layer_rendering,
+            self.cookBook,
+            self.layerDefinition,
+            self.cmf,
+            self.event,
             versionNumber=1
         )
         self.assertIsInstance(mc, MapChef)
 
     def test_map_chef_cook(self):
         my_mxd = arcpy.mapping.MapDocument(self.my_mxd_fname)
+        productName = "Example Map"
 
         mc = MapChef(
             my_mxd,
-            self.cmf.map_definitions,
-            self.cmf.layer_properties,
-            self.path_to_valid_cmf_des,
-            self.cmf.layer_rendering,
+            self.cookBook,
+            self.layerDefinition,
+            self.cmf,
+            self.event,
             versionNumber=1
         )
-
         try:
-            mc.cook("Example Map", "FICTION-LAND")
+            mc.cook(self.cookBook.products[productName])
             self.assertTrue(True)
         except Exception as e:
             self.fail(e)
